7e6f2b87795141affc9f2dfd7905032682af64fb
fre <arthgagdfg@gmail.com>
2026-05-25 21:13:38 -0300

    Adicionar arquivos de projeto.

---
+++ Relacionamentos/Data/BlogContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Relacionamentos.Models;
+
+namespace Relacionamentos.Data
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options) { }
+
+        public DbSet<Blog> Blogs => Set<Blog>();
+        public DbSet<Post> Posts => Set<Post>();
+    }
+}
